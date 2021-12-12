e7475c09d059fb5e4cb70c1e9735e725217bb700
thanhtu179 <thanhtu14693@gmail.com>
2021-12-12 11:17:45 +0900

    setup router

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from "../assets/images/Logo-2.png"
+
+
+const Header = () => {
+    return (
+        <div>
+            <img src={logo} alt=".."/>
+        </div>
+    )
+}
+
+export default Header
