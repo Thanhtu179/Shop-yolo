f25ecce35c0f18b0fa61b365ce0670e37af558fd
thanhtu179 <thanhtu14693@gmail.com>
2021-12-14 14:42:06 +0900

    make product-card component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 import Helmet from '../components/Helmet'
 import HeroSlider from '../components/HeroSlider'
 import heroSliderData from '../assets/fake-data/hero-slider'
@@ -6,7 +8,8 @@ import Section, { SectionTitle, SectionBody } from "../components/Section"
 import policy from "../assets/fake-data/policy"
 import PolicyCard from '../components/PolicyCard'
 import Grid from '../components/Grid'
-import { Link } from 'react-router-dom'
+import ProductCard from '../components/ProductCard'
+import productData from "../assets/fake-data/products"
 
 const Home = () => {
     return (
@@ -19,22 +22,22 @@ const Home = () => {
                 timeOut={5000}
             />
             {/* End hero slider */}
+
             {/* Policy section */}
             <Section>
                 <SectionBody>
                     <Grid
                         col={4}
-                        mlCol={2}
+                        mdCol={2}
                         smCol={1}
                         gap={20}
                     >
                         {
-                            policy.map((item, index) => <Link to="/">
+                            policy.map((item, index) => <Link key={index} to="/policy">
                                 <PolicyCard
-                                key={index}
-                                name={item.name}
-                                description={item.description}
-                                icon={item.icon}
+                                    name={item.name}
+                                    description={item.description}
+                                    icon={item.icon}
                                 />
                             </Link>)
                         }
@@ -42,6 +45,35 @@ const Home = () => {
                 </SectionBody>
             </Section>
             {/* End Policy section */}
+
+            {/* best sellings section */}
+            <Section>
+                <SectionTitle>
+                    top sản phẩm bán chạy trong tuần
+                </SectionTitle>
+                <SectionBody>
+                <Grid
+                        col={4}
+                        mdCol={2}
+                        smCol={1}
+                        gap={20}
+                    >
+                        {
+                            productData.getProducts(4).map((item, index) => (
+                                <ProductCard
+                                    key={index}
+                                    img01={item.image01}
+                                    img02={item.image02}
+                                    name={item.title}
+                                    price={Number(item.price)}
+                                    slug={item.slug}
+                                />
+                            ))
+                        }
+                    </Grid>
+                </SectionBody>
+            </Section>
+            {/* End best sellings section */}
 
         </Helmet>
     )
